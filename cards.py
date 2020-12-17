5400e8055e4d99b06d835859c6650afe2c51fa45
esrafiliyanm <esrafiliyanm@gmail.com>
2020-12-17 17:44:36 +0330

    cards modified and part added

--- cards.py
+++ cards.py
@@ -10,3 +10,11 @@ class Card:
 
     def __str__(self):
         return 'Card (%d , %s)' % (self.number, self.color)
+
+    def __eq__(self , other):
+        if not isinstance(other, Card):
+            return False
+        return (self.number == other.number and self.color == other.color)
+
+    def __hash__(self):
+        return hash((self.number , self.color))
